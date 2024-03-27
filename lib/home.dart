af6640550e9d73c49f7b008649ef695739aa8da2
tomnn1608 <nxt16082004@gmail.com>
2024-03-27 16:30:53 +0700

    HomeScreene

---
+++ lib/home.dart
@@ -0,0 +1,48 @@
+import 'package:exam4/widget/widget_support.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.only(top: 50.0, left: 20.0, right: 10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("English Academy", style: AppWidget.boldTextFeildStyle()),
+                Container(
+                  padding: EdgeInsets.all(3),
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Icon(
+                    Icons.shopping_cart,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text("Uy Tin Chat Luong",
+                style: AppWidget.HeadlineTextFeildStyle()),
+            Text("Top 1 Viet Nam",
+                style: AppWidget.LightTextFeildStyle()),
+          ],
+        ),
+      ),
+    );
+  }
+}
